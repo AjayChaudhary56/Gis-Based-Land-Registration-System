c48f2798dd5efebe4952287fcd41b2b0798e4397
Ajay Chaudhary <iamazoy110125@gmail.com>
2022-12-15 09:48:34 +0545

    all basic files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './pagination';
+import {BrowseRouter as Router, Routes} from "react-router-dom";
+// import Search from './search';
+// import Stories from './stories';
+// import {useGlobalContext} from './context'
 function App() {
+  // const data = useGlobalContext();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <div>Hello {data} </div> */}
+{/* <Search/> */}
+<Router>
+<Routes>
+
+
+<Navbar/>
+ {/* <Stories/> */}
+
+
+</Routes>
+</Router>
+    </>
   );
 }
 
