84f80024d81f4acd371f0b7496697188b58a108d
Ajay Chaudhary <iamazoy110125@gmail.com>
2022-12-26 12:07:29 +0000

    starting phase1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes></Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
